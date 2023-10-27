c31d4ffdc4ca1e1740d742f726a85223fee38920
Greg Cannon <gregccannon@gmail.com>
2023-10-26 19:20:49 -0700

    Add micro web server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cModule = require("./build/Release/cRabbit");
 
+// Simple test run
+
 function cTest() {
   const result = cModule.ping();
   console.log(result);
@@ -8,3 +10,20 @@ function cTest() {
 console.log("Making C++ module call");
 cTest();
 console.log("Done C++ module call");
+
+// Simple web server
+const http = require("http");
+const port = 3000;
+
+const simpleServer = http.createServer((req, res) => {
+  const result = "C++ reponse: " + cModule.ping();
+  res.end(result);
+})
+
+simpleServer.listen(port, (error) => {
+  if (error){
+    console.log("Something went wrong:", error);
+  } else {
+    console.log("Listening on port:", port);
+  }
+})
